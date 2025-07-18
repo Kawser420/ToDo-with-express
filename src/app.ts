55eaa7f54a54571addf7eb5ef3305fe7c9c68eb4
Kawser420 <muhammadkawser202@gmail.com>
2025-07-18 22:47:21 +0600

    sever file setup and ready

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
-import express, { Application, Request, Response } from "express";
+import { Application, Request, Response } from "express";
+
+const express = require("express");
 const app: Application = express();
-const port = 3000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.get("/todos", (req: Request, res: Response) => {
+  res.send("Hello World!");
 });
+
+app.get("/todos/create-todo", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
-import app from "app";
-const port = 3000;
+import app from "./app";
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+let server;
 
-export default app;
+const port = 5000;
+
+const bootstrap = async () => {
+  server = app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+};
+
+bootstrap();
