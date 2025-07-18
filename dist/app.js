dce68596ebd582237b40d13584e0f4b3c31fb46f
Kawser420 <muhammadkawser202@gmail.com>
2025-07-19 00:22:21 +0600

    good perform

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,21 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
 const app = express();
+const filepath = path_1.default.join(__dirname, "../db/todo.json");
 app.get("/", (req, res) => {
+    res.send("how to become a real world full sta international and to be happy");
+});
+app.get("/todos", (req, res) => {
+    const data = fs_1.default.readFileSync(filepath, { encoding: "utf-8" });
+    console.log(data);
+    res.send("Hello World!");
+});
+app.get("/todos/create-todo", (req, res) => {
     res.send("Hello World!");
 });
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,18 @@
 import { Application, Request, Response } from "express";
+import fs from "fs";
+import path from "path";
 
-const express = require("express");
 const app: Application = express();
 
+const filepath = path.join(__dirname, "../db/todo.json");
+
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  res.send("how to become a real world full sta international and to be happy");
 });
 
 app.get("/todos", (req: Request, res: Response) => {
+  const data = fs.readFileSync(filepath, { encoding: "utf-8" });
+  console.log(data);
   res.send("Hello World!");
 });
 
