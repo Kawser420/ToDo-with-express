ba9ffaffa711a19cf97451a012ed949638197a4c
Kawser420 <muhammadkawser202@gmail.com>
2025-07-19 11:18:57 +0600

    use middleware

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
-import { Application, Request, Response } from "express";
+import express, { Application, Request, Response } from "express";
 import fs from "fs";
 import path from "path";
 
 const app: Application = express();
+
+app.use(express.json());
 
 const filepath = path.join(__dirname, "../db/todo.json");
 
@@ -13,10 +15,12 @@ app.get("/", (req: Request, res: Response) => {
 app.get("/todos", (req: Request, res: Response) => {
   const data = fs.readFileSync(filepath, { encoding: "utf-8" });
   console.log(data);
-  res.send("Hello World!");
+  res.json(data);
 });
 
-app.get("/todos/create-todo", (req: Request, res: Response) => {
+app.post("/todos/create-todo", (req: Request, res: Response) => {
+  const { title, body } = req.body;
+  console.log(title, body);
   res.send("Hello World!");
 });
 
