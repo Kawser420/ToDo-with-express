f2f0a14b493083010e7cc2cdb66e2e756a0558d2
Kawser420 <muhammadkawser202@gmail.com>
2025-07-20 11:50:12 +0600

    Creating & Reading ToDos

--- src/app/todos/todos.routers.ts
+++ src/app/todos/todos.routers.ts
@@ -1,25 +1,37 @@
 import express, { Request, Response } from "express";
 import fs from "fs";
 import path from "path";
+import { client } from "../../config/mongodb";
 
 const filepath = path.join(__dirname, "../../../db/todo.json");
 
 export const todosRouter = express.Router();
 
-todosRouter.get("/", (req: Request, res: Response) => {
-  const data = fs.readFileSync(filepath, { encoding: "utf-8" });
+todosRouter.get("/", async (req: Request, res: Response) => {
+  const db = await client.db("todosDB");
+  const collection = await db.collection("todos");
 
-  console.log("ToDos router");
-  res.json({
-    message: "From Todos Router",
-    data,
-  });
+  const cursor = collection.find({});
+  const todos = await cursor.toArray();
+
+  res.json(todos);
 });
 
-todosRouter.post("/create-todo", (req: Request, res: Response) => {
-  const { title, body } = req.body;
-  console.log(title, body);
-  res.send("Hello World!");
+todosRouter.post("/create-todo", async (req: Request, res: Response) => {
+  const { title, description, priority } = req.body;
+
+  const db = await client.db("todosDB");
+  const collection = await db.collection("todos");
+  await collection.insertOne({
+    title: title,
+    description: description,
+    priority: priority,
+  });
+
+  const cursor = collection.find({});
+  const todos = await cursor.toArray();
+
+  res.json(todos);
 });
 
 todosRouter.get("/:title", (req: Request, res: Response) => {
