1a12bac6ccf0682715cd85c03d3e5c6111a69bc6
Kawser420 <muhammadkawser202@gmail.com>
2025-07-20 15:50:20 +0600

    finished ToDo with express

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import { todosRouter } from "./app/todos/todos.routers";
 const app: Application = express();
 
@@ -7,10 +7,62 @@ app.use(express.json());
 const userRouter = express.Router();
 
 app.use("/todos", todosRouter);
-app.use("/user", userRouter);
+app.use("/users", userRouter);
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Welcome To Express Tour");
+app.get(
+  "/",
+  (req: Request, res: Response, next: NextFunction) => {
+    console.log({
+      url: req.url,
+      method: req.method,
+      header: req.header,
+    });
+    next();
+  },
+
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      res.send("Welcome to Todos App");
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+app.get(
+  "/error",
+
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      res.send("Welcome to error er world");
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({ message: "Route not found" });
 });
 
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  if (error) {
+    console.log("error", error);
+    res.status(400).json({
+      message: "Something went wrong from global error handler",
+      error,
+    });
+  }
+});
+
+// [app]-[express.json()]-[todosRouter]-[Root Route "/"]-[GET "/todos"]-[POST Create ToDo]
+//[todosRouter]-[get all todos /todos GET]-[create todo /todos/create-todo POST todo]
+
 export default app;
+
+/**
+ * Basic File structure
+ * server - server handling like - starting, closing error handling of server. only related to server
+ * app file - routing handle, middleware, route related error
+ * app folder - app business logic handling like create read update delete, database related works
+ */
