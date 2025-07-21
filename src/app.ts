77a184516dd2093ac414fe6055be10f0634109dc
Kawser420 <muhammadkawser202@gmail.com>
2025-07-21 20:08:47 +0600

    some code updated now

--- src/app.ts
+++ src/app.ts
@@ -61,7 +61,7 @@ app.use((error: any, req: Request, res: Response, next: NextFunction) => {
 export default app;
 
 /**
- * Basic File structure
+ * Basic File structure.
  * server - server handling like - starting, closing error handling of server. only related to server
  * app file - routing handle, middleware, route related error
  * app folder - app business logic handling like create read update delete, database related works
